7fb66d3a65ec99ef40c22c84230cea2f67b5c100
Shogo Sawai <shogo.sawai@gmail.com>
2020-03-18 12:36:46 +0900

    add skelton

---
+++ src/app.tsx
@@ -0,0 +1,8 @@
+import { hot } from 'react-hot-loader/root'
+import * as React from 'react'
+
+const App = () => {
+    return <div>hello world</div>
+}
+
+export default hot(App)
---
+++ webpack.common.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
+module.exports = {
+  entry: [
+      './src/index',
+  ],
+  resolve: {
+    modules: ['node_modules'],
+    alias: {
+      'react-dom': '@hot-loader/react-dom',
+    },
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(j|t)s(x)?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true,
+            babelrc: true,
+          },
+        },
+      },
+    ],
+  },
+  devtool: 'eval-source-map',
+  plugins: [
+      new ForkTsCheckerWebpackPlugin(),
+  ],
+};
